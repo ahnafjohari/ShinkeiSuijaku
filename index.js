0f276e0f1730aaddbc38290afc00d684c7be13cb
Ahnaf Johari <ahnaf58@gmail.com>
2025-10-09 21:42:01 +0800

    Add more flags

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ function initGame() {
 
 	// 3️⃣ Define the cards data
 	const flags = [
+		{ name: 'argentina', img: 'images/argentina.svg' },
+		{ name: 'belgium', img: 'images/belgium.svg' },
+		{ name: 'brazil', img: 'images/brazil.svg' },
+		{ name: 'china', img: 'images/china.svg' },
+		{ name: 'england', img: 'images/england.svg' },
 		{ name: 'japan', img: 'images/japan.svg' },
 		{ name: 'malaysia', img: 'images/malaysia.svg' }
 	];
